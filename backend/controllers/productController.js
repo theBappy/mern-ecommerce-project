4bfc01d1d232abbfedf2a412def2051a6bfee66d
theBappy <abappy575@gmail.com>
2025-04-05 16:57:53 -0300

    admin middleware for admin authorization

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -32,7 +32,7 @@ const addProduct = async(req,res)=>{
         image: imagesUrl,
         date: Date.now()
     }
-    console.log(productData)
+    
 
     const product = new productModel(productData)
     
@@ -49,17 +49,37 @@ const addProduct = async(req,res)=>{
 
 // list products
 const listProduct = async(req,res)=>{
-
+   try{
+    const products = await productModel.find({})
+    res.json({success: true, products})
+   }catch(error){
+    console.log(error)
+    res.json({success:false, message:error.message})
+   }
 }
 
 // remove or delete a  product
 const removeProduct = async(req,res)=>{
-
+  try{
+    await productModel.findByIdAndDelete(req.body.id)
+    res.json({success:true, message: 'Product deleted successfully'})
+  }catch(error){
+    console.log(error)
+    res.json({success:false, message:error.message})
+  }
 }
 
 // single product info
 const singleProduct = async(req,res)=>{
-
+    try{
+    const { productId } = req.body;
+    const product = await productModel.findById(productId)
+    res.json({success:true,product})
+    }catch(error){
+      console.log(error)
+      res.json({success:false, message:error.message})
+    }
+    
 }
 
 
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,18 @@ const registerUser = async(req,res) => {
 
 // route controller for admin log in only
 const adminLogin = async(req,res) => {
-    
+    try{
+        const {email, password} = req.body;
+        if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true, token})
+        }else{
+            res.json({success:false, message:'Invalid credentials'})
+        }
+    }catch(error){
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
 }
 
 export {loginUser, registerUser, adminLogin};
