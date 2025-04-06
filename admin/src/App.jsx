0af55668af3a2871a681d00ef5d5789be6d25467
theBappy <abappy575@gmail.com>
2025-04-06 14:42:24 -0300

    list product for admin panel done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,6 +11,8 @@ import { ToastContainer } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
+export const currency = '$'
+
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
 
