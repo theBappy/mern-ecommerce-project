02e61bebd98ebbbd814a0d300aa4a54c1456b2e2
theBappy <abappy575@gmail.com>
2025-04-06 13:58:52 -0300

    product add from admin panel done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import AddProduct from "./pages/AddProduct";
+import ListProduct from "./pages/ListProduct";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+import { ToastContainer } from 'react-toastify';
+
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):'');
+
+  useEffect(()=>{
+   localStorage.setItem('token', token)
+  },[token])
+
   return (
-    <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr className="text-gray-300" />
-        <div className="flex w-full">
-          <Sidebar />
-        </div>
-      </>
+    <div className="bg-[#ececec] min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr className="text-gray-300" />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<AddProduct token={token} />} />
+                <Route path="/list" element={<ListProduct token={token} />} />
+                <Route path="/orders" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
   return (
     <div className='w-[18%] min-h-screen border-r-2 border-gray-300'>
         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
-            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-1' to="/add">
+            <NavLink className='flex items-center gap-3 border border-gray-500 border-r-0 px-3 py-2 rounded-1' to="/add">
                 <img src={assets.add_icon} alt="add_icon" className='w-5 h-5' />
                 <p className='hidden md:block'>Add Items</p>
             </NavLink>
